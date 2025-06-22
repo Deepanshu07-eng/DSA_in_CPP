d6d897d5cac393eda9ffbc28dfc715848c0692eb
Deepanshu07-eng <d2004gautam@gmail.com>
2025-06-22 11:24:46 +0530

    loops amd functions

---
+++ CB lectures/Functions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//1.Reusability
+//2. more structured code and readable
+//3. function Declaration : void helloworld()
+
+void helloworld(){
+    cout<<"HelloWorld!\n";
+}
+
+int main(){
+    helloworld();//invoking or calling
+    return 0;
+}
