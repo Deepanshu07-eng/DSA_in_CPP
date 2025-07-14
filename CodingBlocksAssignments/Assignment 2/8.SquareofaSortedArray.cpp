7c848c65c48eead2b1ad06ba2be396ecd326182c
Deepanshu07-eng <d2004gautam@gmail.com>
2025-07-14 20:49:11 +0530

    did some more questions

---
+++ CodingBlocksAssignments/Assignment 2/8.SquareofaSortedArray.cpp
@@ -0,0 +1,27 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+
+    int val = 0;
+    for(int i=0; i<n; i++){
+        a[i] = a[i] * a[i];
+        
+    }
+    sort(a, a+n);
+     for(int i=0; i<n; i++){
+        cout<<a[i]<<' ';
+    }
+    
+    
+
+    return 0;
+}
