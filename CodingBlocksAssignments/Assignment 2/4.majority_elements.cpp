b1ee24aacbb8d33279442f4c1bc8c1ffe02d69f9
Deepanshu07-eng <d2004gautam@gmail.com>
2025-07-15 22:34:06 +0530

    majority element

--- CodingBlocksAssignments/Assignment 2/4.majority_elements.cpp
+++ CodingBlocksAssignments/Assignment 2/4.majority_elements.cpp
@@ -2,3 +2,25 @@
 
 //You may assume that the array is non-empty and the majority element always exist in the array.
 
+#include<iostream>
+using namespace std;
+int main () {
+
+	int n;
+	cin>>n;
+
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin>>arr[i];
+	}
+	int freq = 0, ans = 0;
+	for (int i = 0; i < n; i++) {
+		if (freq == 0) ans = arr[i];
+
+		freq += (arr[i] == ans) ? 1 : -1;
+	}
+
+	cout<<ans;
+	return 0;
+}
