0a7dc6283038a7bdb7f9e44e4e676f3666e123a8
Deepanshu07-eng <d2004gautam@gmail.com>
2025-07-19 10:52:25 +0530

    Delimeter

--- CB lectures/Char_array.cpp
+++ CB lectures/Char_array.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main(){
     char a[100];
+   // cin.getline(a, 5, '.');
+   cin.getline(a, 100); //delimiter can turn to \n 
+
+    cout<<a<<endl;
+
+    /*char a[100];
     cin>>a;
 
-    cout<<a;
+    cout<<a;*/
 
     return 0;
 }
---
+++ CB lectures/CustomInputString.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void inputstring(char *a, int N, char delimiter = '\n' ){
+    int i=0;
+    char ch =cin.get();
+
+    while(i<N-1 && ch!= delimiter){
+        a[i]=ch;
+        i++;
+
+        ch =cin.get();
+    }
+    a[i]='\0';
+
+}
+
+int main(){
+    char a[100];
+    inputstring(a, 100, '#');
+    //cin.getline(a,100);
+    cout<<a<<endl;
+    }
