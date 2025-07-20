e64926582d8fdbcd1e25d076e57ae460d5256547
Deepanshu07-eng <d2004gautam@gmail.com>
2025-07-20 11:21:27 +0530

    Sub String

---
+++ CB lectures/Substrings.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+
+    char a[]= "abcd";
+    int lena = strlen(a);
+
+    for(int i=0; i<lena; i++){
+        for(int j=i; j<lena; ++j){
+            for(int k=i; k<=j; ++k){
+                cout<<a[k];
+            }
+            cout<<endl;
+
+         }
+         cout<<endl;
+    }
+    return 0;
+}
