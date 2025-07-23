42107ab4ee1a1fd2302600791b0ef94aae806838
Deepanshu07-eng <d2004gautam@gmail.com>
2025-07-23 23:52:10 +0530

    Some more pattern question practice

--- PatternQuestions/pattern13.cpp
+++ PatternQuestions/pattern13.cpp
@@ -1,27 +1,27 @@
 /*
-Enter n: 5
-*
-* *
-* * *
+if Enter n :5
+* * * * * 
 * * * *
-* * * * *
+* * *
+* *
+*
 
 */
-
 
 #include <iostream>
 using namespace std;
 
 int main() {
     int n;
-    cout<<"Enter n: ";
+    cout<<"Enter n :";
     cin>>n;
 
-    for(int i =1; i<=n; i++){
-        for(int j=1; j<=i; j++ ){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1;  j++){
             cout<<"* ";
         }
         cout<<endl;
+
     }
     return 0;
 }
