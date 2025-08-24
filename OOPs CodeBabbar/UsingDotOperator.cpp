3fce3c18aff58fee6176ce4416b5b4338c23fa0c
Deepanshu <d2004gautam@gmail.com>
2025-08-24 20:08:27 +0530

    Access Modifiers

--- OOPs CodeBabbar/UsingDotOperator.cpp
+++ OOPs CodeBabbar/UsingDotOperator.cpp
@@ -6,20 +6,21 @@ class Hero{
     public: // accessable everywhere
     int Health;
     char level;
-    private: // Only accessable in Class
+    //private: // Only accessable in Class
     int Height;
-
-    void print(){
-        cout<<Height<<endl;
-    }
 };
 
 int main(){
     //Creation of Object
     Hero Rahul;
 
+    Rahul.Health = 98;
+    Rahul.level = 'A' ;
+    Rahul.Height = 5'11;
+
+
     cout<<"Health is: " << Rahul.Health<<endl;
     cout<<"level is: " << Rahul.level<<endl;
 
-    
-}
+    return 0;
+};
