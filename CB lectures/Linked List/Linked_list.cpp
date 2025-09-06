35dacbae26a46f0f082a2d8a6cf2e0b518030626
Deepanshu <d2004gautam@gmail.com>
2025-09-06 11:24:13 +0530

    Linked-List

---
+++ CB lectures/Linked List/Linked_list.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node* next;
+
+};
+
+void insertatstart(){
+
+}
+
+int main(){
+    node* head, *tail;
+    head = tail =NULL; // be default pointers me garbage nhi chhodte
+
+    return 0;
+}
