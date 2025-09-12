5320661bcbc4c2fa7b96c0aa0ecd2355c86df693
Deepanshu <d2004gautam@gmail.com>
2025-09-12 23:05:09 +0530

    A boolean matrix

---
+++ CodingBlocksAssignments/Assignment 4/Q8-A_boolean_matrix.cpp
@@ -0,0 +1,48 @@
+/* 
+Given a boolean matrix mat[M][N] of size M X N, modify it such that if a matrix cell mat[i][j] is 1 (or true) then make all the cells of ith row and jth column as 1.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    vector<vector<int>> mat(m, vector<int>(n));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    vector<bool> row(m, false);
+    vector<bool> col(n, false);
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mat[i][j] == 1) {
+                row[i] = true;
+                col[j] = true;
+            }
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (row[i] || col[j]) {
+                mat[i][j] = 1;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
