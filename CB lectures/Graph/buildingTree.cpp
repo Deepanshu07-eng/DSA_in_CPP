ac2f8d09a9989e72cd7820fb736220ae5dcf458a
Deepanshu <d2004gautam@gmail.com>
2025-09-20 11:13:46 +0530

    Diameter of tree

--- CB lectures/Graph/buildingTree.cpp
+++ CB lectures/Graph/buildingTree.cpp
@@ -57,6 +57,16 @@ int height(node* root){
 
     return max(height(root->left), height(root->right))+1;
 }
+
+int diameter(node* root){
+    if(!root) return 0;
+    int op1 = height(root->left) + height(root->right);
+    int op2 = diameter(root->left);
+    int op3 = diameter(root->right);
+
+    return max(op1, max(op2,op3));
+
+}
 //8 10 1 -1 -1 6 4 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
 int main() {
     cout<<"Enter input: ";
@@ -75,5 +85,6 @@ int main() {
 
     cout<<"Total nodes = "<<countNode(root)<<endl;
     cout<<"Tree Height = "<<height(root)<<endl;
+    cout<<"Tree Diameter = "<<diameter(root)<<endl;
     return 0;
 }
