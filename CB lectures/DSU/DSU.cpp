0b3746d9a88f97bbe9c95c2f174d211bc28b47ba
Deepanshu <d2004gautam@gmail.com>
2025-10-21 14:48:43 +0530

    Vector

---
+++ CB lectures/DSU/DSU.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class DSU {
+public:
+
+int* parent ;
+int n;
+DSU(int N){
+    n = N +1;
+    parent = new int (N+1);
+    for(int i =0; i<=N; i++){
+
+        parent[i]= i;
+    }
+}
+int get_leader(int u){
+    if(u == parent[u] ) return u;
+    return get_leader(parent[u]);
+}
+
+int union_leader(int u, int v){
+    int leader_u = get_leader(u);
+    int leader_v = get_leader(v);
+
+    if(leader_u != leader_v){
+        parent[leader_v] = leader_u;
+
+    }
+}
+};
+
+
+
+int main() {
+
+    return 0;
+}
---
+++ CB lectures/Vector/V.Code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec= {1,2,3}; //0
+
+    cout<<vec[2]<<endl;
+
+    return 0;
+}
