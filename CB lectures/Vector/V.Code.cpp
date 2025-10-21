7f0af7fe78dada1c1c231ac9f7abdbb582396f56
Deepanshu <d2004gautam@gmail.com>
2025-10-21 14:50:27 +0530

    Vector

--- CB lectures/Vector/V.Code.cpp
+++ CB lectures/Vector/V.Code.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main() {
-    vector<int> vec= {1,2,3}; //0
+    vector<int> vec(5,0); //0
 
+    cout<<vec[0]<<endl;
+    cout<<vec[1]<<endl;
     cout<<vec[2]<<endl;
+    cout<<vec[3]<<endl;
+    cout<<vec[4]<<endl;
 
     return 0;
 }
