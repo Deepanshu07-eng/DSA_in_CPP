acac7d98b4a31da358a8426577ff5059708cd22b
Deepanshu <d2004gautam@gmail.com>
2026-01-02 21:23:47 +0530

    Friends pairing problem

---
+++ CodingBlocksAssignments/Assignment 9/21_Friends Pairing Problem.cpp
@@ -0,0 +1,23 @@
+/*
+Given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. Find out the total number of ways in which friends can remain single or can be paired up.
+
+
+Input Format
+First line contains an integer t denoting the number of test cases. Next t lines contain an integer n each.
+
+
+Constraints
+1<= n < 30
+
+
+Output Format
+Output t lines each line describing the answer.
+
+
+Sample Input
+1
+3
+Sample Output
+4
+*/
+
